d70367774e07796caf4bfde78195637b9138d0de
Gekif <dzulfikar.maulana@gmail.com>
2020-05-19 05:14:22 +0700

    04 The Framework [Part 2] - MVC Workflow: 025 Clean Up

--- traversymvc/app/views/pages/index.php
+++ traversymvc/app/views/pages/index.php
@@ -2,10 +2,6 @@
 
 <h1><?php echo $data['title']; ?></h1>
 
-<ul>
-    <?php foreach ($data['posts'] as $post) : ?>
-        <li><?php echo $post->title ?></li>
-    <?php endforeach; ?>
-</ul>
+<p>This is the TraversyMVC PHP Framework. Please Refer to the Docs On How To Use It</p>
 
 <?php require APPROOT . '/views/inc/footer.php' ?>
