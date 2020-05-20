c895e02f1189f08e56117327651b7669c73acfd5
Gekif <dzulfikar.maulana@gmail.com>
2020-05-20 17:33:50 +0700

    05 The App [1] - Setup & User Authentication: 026 Initial App & Database Setup

--- traversymvc/app/views/pages/index.php
+++ traversymvc/app/views/pages/index.php
@@ -2,6 +2,4 @@
 
 <h1><?php echo $data['title']; ?></h1>
 
-<p>This is the TraversyMVC PHP Framework. Please Refer to the Docs On How To Use It</p>
-
 <?php require APPROOT . '/views/inc/footer.php' ?>
