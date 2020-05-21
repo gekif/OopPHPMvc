3ce008bc64b41ccf4bc58e5ecf532d6e8e63a976
Gekif <dzulfikar.maulana@gmail.com>
2020-05-21 10:08:41 +0700

    05 The App [1] - Setup & User Authentication: 031 User Model & Email Check

--- traversymvc/app/controllers/Users.php
+++ traversymvc/app/controllers/Users.php
@@ -4,7 +4,7 @@ class Users extends Controller
 {
     public function __construct()
     {
-
+        $this->userModel = $this->model('User');
     }
 
 
@@ -32,6 +32,11 @@ class Users extends Controller
             // Validate Email
             if (empty($data['email'])) {
                 $data['email_err'] = 'Please enter email';
+            } else {
+                // Check email
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
 
             // Validate Name
