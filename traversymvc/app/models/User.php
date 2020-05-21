230063dd9fd27cf37e31661a2b0777d4a76aea80
Gekif <dzulfikar.maulana@gmail.com>
2020-05-21 10:22:52 +0700

    05 The App [1] - Setup & User Authentication: 032 User Registration

--- traversymvc/app/models/User.php
+++ traversymvc/app/models/User.php
@@ -11,11 +11,32 @@ class User
     }
 
 
+    // Register User
+    public function register($data)
+    {
+        $this->db->query('INSERT INTO users (name, email, password, created_at) VALUES (:name, :email, :password, :created_at)');
+
+        // Bind Values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':created_at', date("Y-m-d H:i:s"));
+
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     // Find user by email
     public function findUserByEmail($email)
     {
         $this->db->query('SELECT * FROM users WHERE email = :email');
 
+        // Bind Value
         $this->db->bind(':email', $email);
 
         $row = $this->db->single();
